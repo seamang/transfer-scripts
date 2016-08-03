8f9466abf5b69095fe28048ade11c9caf54d6619
gseaman <gseaman@nationalarchives.gov.uk>
2016-08-03 11:21:54 +0100

    Append date to all crawl folders and rewrite metadata to match

---
+++ modifyMetadataFolderNames.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+"""
+Add the crawl start date to each folder in the metadata, and move the
+actual folder correspondingly
+"""
+
+from getopt import getopt, GetoptError
+from csv import DictWriter, DictReader
+import sys, re, requests, time, random
+from datetime import datetime
+from time import strftime
+from hurry.filesize import size
+from os import stat
+from shutil import move
+
+
+IDENTIFIER_BASE = 'file:///T:WORK/RW_32/content/'
+
+def main(argv):
+    mountpoint, ifname, ofname = getParms()
+    # try opening the files    
+    try:
+        #scl enable python27 bash
+        # to allow multiple openings on one line
+        with open(ifname, "rb") as mi, open(ofname, "wb") as mo:
+            fields = ['identifier','filename','folder','date_created','checksum', \
+                      'series_number','creating_body','crawl_start', 'crawl_end', \
+                      'filesize', 'unit']
+            reader = DictReader(mi, fieldnames=fields)
+            writer = DictWriter(mo, delimiter=',', fieldnames=fields)
+            writer.writerow(dict((fn,fn) for fn in fields))
+            print "[INFO] Opened files successfully."
+            modifyMetadata(mountpoint, reader, writer)
+    except IOError as e:
+        print "[IOERROR] " + e
+
+def modifyMetadata(mountpoint, reader, writer):
+
+    # default start_date
+    start_date = ''
+    # skip header row
+    next(reader, None)
+    for row in reader:
+        if row['folder'] == 'folder':
+            start_date = row['crawl_start'].split("T")[0]
+            old_identifier = row['identifier']
+            row['identifier'] = row['identifier'] + '_' + start_date
+            row['filename'] = row['filename'] + '_' + start_date
+            # TODO move the folder!
+            old_identifier = old_identifier.replace('file:///T:WORK/', mountpoint)
+            new_identifier = row['identifier']
+            new_identifier = new_identifier.replace('file:///T:WORK/', mountpoint)
+            print old_identifier + ' -> ' + new_identifier
+            move(old_identifier, new_identifier)
+            writer.writerow(row)
+        else:
+            identifier = row['identifier']
+            bits = row['identifier'].split('/')
+            bits[len(bits)-2] += '_' + start_date
+            row['identifier'] = "/".join(bits)
+            writer.writerow(row)
+            
+def getParms():
+    """
+    Get command line parameters.
+    """
+    mountpoint = ifile = ofile = ""
+    try:
+        myopts, args = getopt(sys.argv[1:],"m:i:o:")
+    except GetoptError as e:
+        print (str(e))
+        usage()
+ 
+    for o, a in myopts:
+        if o == '-m':
+            mountpoint = a
+        elif o == '-i':
+            ifile = a
+        elif o == '-o':
+            ofile = a
+
+    if not (mountpoint and ifile and ofile):
+        usage()
+
+    return (ifile, ofile)
+
+def usage():
+    print("Usage: %s -m mountpoint -i inputmetadata  -o outputmetadata" % sys.argv[0])
+    sys.exit(2)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
